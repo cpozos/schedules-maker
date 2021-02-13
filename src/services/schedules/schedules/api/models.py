12b6675d3bbd307aae6ca62a8266b2ffedfa9e50
Carlos Alberto Pozos Villegas <cpozosvillegas@hotmail.com>
2021-02-13 14:23:44 -0600

    Display value for Subject established

--- src/services/schedules/schedules/api/models.py
+++ src/services/schedules/schedules/api/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Subject(models.Model):
     name = models.CharField(max_length=200, unique=True)
 
+    def __str__(self):
+        return self.name
+
 class Degree(models.Model):
     name = models.CharField(max_length=200, unique=True)
     num_periods = models.PositiveIntegerField()
